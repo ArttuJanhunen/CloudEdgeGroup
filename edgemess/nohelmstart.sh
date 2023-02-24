0d6ce0141176e24cf1b7e41f4e94e951163551ba
rundtjan <jan.rundt@gmail.com>
2023-02-24 12:03:50 +0000

    evaluation

--- edgemess/nohelmstart.sh
+++ edgemess/nohelmstart.sh
@@ -5,3 +5,4 @@ kubectl apply -f edgemess/templates/nextcloud
 kubectl apply -f edgemess/templates/postgres
 kubectl apply -f edgemess/templates/redis
 kubectl apply -f edgemess/templates/ingress.yaml
+kubectl apply -f edgemess/templates/mariadb
---
+++ evaluation/meter.py
@@ -0,0 +1,20 @@
+import psutil
+import time
+import numpy as np
+
+cpu_use = []
+mem_use = []
+
+#Measuring the cpu and memory-utilization every hundreth of a second for 90 seconds
+for i in range(120000):
+   cpu_use.append(psutil.cpu_percent())
+   mem_use.append(psutil.virtual_memory().percent)
+   time.sleep(0.001)
+
+#Converting into numpy-arrays and using numpy-saving function to create plot in different file (see plot.ipynb)
+cpu = np.asarray(cpu_use)
+mem = np.asarray(mem_use)
+with open('result_with_kube2.npy', 'wb') as f:
+   np.save(f, cpu)
+   np.save(f, mem)
+print(cpu_use)
